e109f626e44a39e26d994de4a109d5e83804ac6d
Lawrence Lockhart <lawrence@vaadin.com>
2023-09-12 17:04:02 -0500

    Upgrade Hilla to 2.2.0, replace @EndPoint with @BrowserCallable

--- src/main/java/com/example/application/endpoints/ParticipantEndpoint.java
+++ src/main/java/com/example/application/endpoints/ParticipantEndpoint.java
@@ -5,13 +5,15 @@ import com.example.application.domain.ParticipantDTO;
 import com.example.application.repositories.ParticipantRepository;
 import com.example.application.services.ParticipantService;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
-import dev.hilla.Endpoint;
+import dev.hilla.BrowserCallable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import static java.util.stream.Collectors.toList;
-@Endpoint
+
+@BrowserCallable
+@Service
 @AnonymousAllowed
 public class ParticipantEndpoint {
 
