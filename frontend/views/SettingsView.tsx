34b170091c190d23c05b17ab00e49339e9873839
Lawrence Lockhart <lawrence@vaadin.com>
2023-10-06 10:39:40 -0500

    Changed to radio buttons, added routing

--- frontend/views/SettingsView.tsx
+++ frontend/views/SettingsView.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react'
 import {useForm} from "@hilla/react-form";
+import {useNavigate} from "react-router-dom";
 
+import ParticipantView from 'Frontend/views/ParticipantView';
 import {ParticipantService} from "Frontend/generated/endpoints";
 import ParticipantDTOModel from "Frontend/generated/com/example/application/services/ParticipantDTOModel";
 import {TextField} from "@hilla/react-components/TextField.js";
@@ -8,13 +10,18 @@ import {EmailField} from "@hilla/react-components/EmailField.js";
 import {Button} from "@hilla/react-components/Button";
 import {Checkbox} from "@hilla/react-components/Checkbox";
 import {CheckboxGroup} from "@hilla/react-components/CheckboxGroup";
+import {RadioGroup} from "@hilla/react-components/RadioGroup";
+import {RadioButton} from "@hilla/react-components/RadioButton";
+
 
 const SettingsView = () => {
-
+    const navigate = useNavigate();
     const {model, field, read, submit} = useForm(ParticipantDTOModel,  {
         onSubmit: async (participant) => {
             const saved = await ParticipantService.save(participant);
             read(saved);
+
+            navigate('/participants');
         }
     })
 
@@ -41,12 +48,10 @@ const SettingsView = () => {
                     <Checkbox value="java" label="Java" {...field(model.technologies)}/>
                     <Checkbox value="javascript" label="Javascript" {...field(model.technologies)}/>
                 </CheckboxGroup>
-                <CheckboxGroup
-                    label="MentorMentee"
-                >
-                    <Checkbox value="mentor" label="mentor" />
-                    <Checkbox value="mentee" label="mentee" />
-                </CheckboxGroup>
+                <RadioGroup>
+                    <RadioButton value="mentor" label="Mentor" />
+                    <RadioButton value="mentee" label="Mentee" />
+                </RadioGroup>
                 <Button onClick={submit} theme="primary">Save</Button>
             </div>
         </div>
